6f35c627b61a6c266d7ee7fee18ae9b0c97b25fb
Jo Minji <mingi2687@gamil.com>
2022-03-02 14:47:00 +0900

    emotion 설치및 사용 예제

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Wrap = styled.div`
+  background: royalBlue;
+`;
 
 export default function Home() {
-  return <div>home</div>;
+  return <Wrap>home</Wrap>;
 }

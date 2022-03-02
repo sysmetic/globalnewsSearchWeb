44c899c536a0ae485454115b14d23ad05c7d6fe2
Jo Minji <mingi2687@gamil.com>
2022-03-02 23:32:38 +0900

    lint 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import Home from "./pages/Home";
 
 function App() {

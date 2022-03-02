17fc12e0c88b21d6e11359dbb5dea2cbd9e67787
Jo Minji <mingi2687@gamil.com>
2022-03-02 17:27:24 +0900

    feat: emotion theme 적용

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Wrap = styled.div`
-  background: royalBlue;
+export default function Home() {
+  return (
+    <HomeWrap>
+      <InnerWrap>
+        <Introduction>글로벌 경제 금융 정보검색 서비스, 모야</Introduction>
+      </InnerWrap>
+    </HomeWrap>
+  );
+}
+
+const HomeWrap = styled.div`
+  height: 1279px;
+  background: linear-gradient(
+    211.86deg,
+    #ffffff -2.13%,
+    rgba(244, 253, 251, 0.896614) 19.27%,
+    rgba(223, 248, 244, 0.7) 49.34%,
+    rgba(253, 221, 210, 0.7) 94.11%
+  );
 `;
 
-export default function Home() {
-  return <Wrap>home</Wrap>;
-}
+const InnerWrap = styled.div`
+  width: 1240px;
+  margin: 0 auto;
+`;
+
+const Introduction = styled.div`
+  font-family: Noto Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 64px;
+  color: ${(props) => props.theme.primaryColor};
+`;

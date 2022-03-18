9d15dca61730de1ed89a43d01d777e474d70d951
seongwon <ink0507@naver.com>
2022-03-18 20:15:41 +0900

    편집페이지 재검토필요하여 삭제

---
+++ src/components/edit/AddKeyword.tsx
@@ -0,0 +1,99 @@
+import styled from "@emotion/styled";
+import CommonContainer from "../layout/CommonContainer";
+import KeywordItem from "./KeywordItem";
+import { useKeywordList } from "../../hooks/useKeywordList";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { RootState } from "../../redux/store";
+import { SetStateAction, useState } from "react";
+import { SearchTitleType } from "../../api/newsListApi";
+import searchKeyword from "../../assets/csvjson.json";
+import { useSearch } from "../../hooks/useSearch";
+
+
+
+const AddKeyword = () => {
+  const keywordList = useKeywordList();
+   const myKeywords = useAppSelector(
+    (state: RootState) => state.keywords
+  );
+  
+  
+  return (
+    <Wrap>
+      <CommonContainer>
+        <KeyWordSearch>
+          <input 
+            type="text"  
+            placeholder="키워드 검색 "/>
+         
+        </KeyWordSearch>
+        <KeywordLine/>
+        <KeyWordTitle>My Keyword</KeyWordTitle>
+        <MyKeywordInner>
+          {myKeywords.map(item => (
+            <KeywordItem key={`mykeyword-${item.data}`} item={item.data}/>
+          ))}
+        </MyKeywordInner>
+      </CommonContainer>
+    </Wrap>
+  )
+}
+
+export default AddKeyword;
+
+const Wrap = styled.div`
+  margin-top: 16px;
+  
+`;
+
+const KeyWordSearch = styled.div`
+
+  input::placeholder {color:#48C0B7;}
+  input::-webkit-input-placeholder {color:#48C0B7;}
+  input:-ms-input-placeholder {color:#48C0B7;}
+  
+  textarea::placeholder {color:#48C0B7;}
+  textarea::-webkit-input-placeholder {color:#48C0B7;}
+  textarea:-ms-input-placeholder {color:#48C0B7;}
+  padding-bottom: 16px;
+  input {
+    border: 1px solid #48C0B7;
+    width: 500px;
+    margin-right: 24px;
+    border-radius: 5px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19px;
+    color: #48C0B7;
+    padding: 11px 0 10px 16px;
+    background-color: rgba(72, 192, 183, 0.05);
+  }
+  input:focus {
+    outline: none;
+  }
+
+`
+
+const KeywordLine = styled.div`
+  width: 516px;
+  border-bottom: 1px solid #c4c4c4;
+`
+
+export const KeyWordTitle = styled.h5`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 30px;
+  color: #48C0B7; 
+  margin-top: 3px;
+`
+
+const MyKeywordInner = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 500px;
+  gap: 20px;
+  padding-top: 16px;
+`;
+function addKeyword(item: string): any {
+  throw new Error("Function not implemented.");
+}
---
+++ src/components/edit/EditContainer.tsx
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled";
+import CommonContainer from "../layout/CommonContainer";
+import KeywordItem from "./KeywordItem";
+import MykeyWordArea from "./MykeyWordArea";
+import { useKeywordList } from "../../hooks/useKeywordList";
+import { useAppDispatch } from "../../redux/hooks";
+
+const EditContainer = () => {
+  const keywordList = useKeywordList();
+  
+  return (
+    <Wrap>
+      <CommonContainer>
+        <KeyWordTitle>My Keyword</KeyWordTitle>
+        <MykeyWordArea />
+        <KeywordListWrap>
+          {keywordList.map((item, index) => (
+            <KeywordList key={index}>
+              <SubTitle>{item.title}</SubTitle>
+              <KeywordArea>  
+                {item.data.map((item,index) => (
+                  <KeywordItem item={item} key={`mykeyword-${item}-${index}`}/>
+                ))}
+              </KeywordArea>
+            </KeywordList>
+          ))}
+        </KeywordListWrap>
+      </CommonContainer>
+    </Wrap>
+  );
+};
+
+export default EditContainer;
+
+const Wrap = styled.main`
+  margin-top: 60px;
+`;
+const KeywordList = styled.div`
+  margin-bottom: 50px;
+`
+
+const KeywordListWrap = styled.div`
+ 
+`;
+
+export const KeyWordTitle = styled.h5`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+`;
+
+const SubTitle = styled.h4`
+  margin: 20px 0;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 22px;
+`;
+
+const KeywordArea = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`
+
+function addKeyword(item: string): any {
+  throw new Error("Function not implemented.");
+}

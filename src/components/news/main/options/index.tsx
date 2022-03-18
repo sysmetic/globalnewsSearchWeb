2849378a7e62dc3e2510cba77b268895b4e110ec
Jo Minji <mingi2687@gamil.com>
2022-03-18 18:55:02 +0900

    이미지 경로 수정

--- src/components/news/main/options/index.tsx
+++ src/components/news/main/options/index.tsx
@@ -45,7 +45,7 @@ const Features = styled.div`
 const TextSizeControl = styled.div`
   width: 40px;
   height: 40px;
-  background-image: url("images/icon-Text-Size.svg");
+  background-image: url("/images/icon-Text-Size.svg");
   background-size: cover;
   background-repeat: no-repeat;
   cursor: pointer;
@@ -61,8 +61,8 @@ const ImageFormatViewBtn = styled.div<Props>`
   cursor: pointer;
   background: ${({ focused }) =>
       focused === true
-        ? "url(images/icon-Grid-filled.svg)"
-        : "url(images/icon-Grid.svg)"}
+        ? "url(/images/icon-Grid-filled.svg)"
+        : "url(/images/icon-Grid.svg)"}
     no-repeat 4.5%;
 `;
 
@@ -71,8 +71,8 @@ const TextFormatViewBtn = styled.div<Props>`
   height: 40px;
   background: ${({ focused }) =>
       focused === true
-        ? "url(images/icon-Grid-list.svg)"
-        : "url(images/icon-hamburger-button.svg)"}
+        ? "url(/images/icon-Grid-list.svg)"
+        : "url(/images/icon-hamburger-button.svg)"}
     no-repeat 4.5%;
   background-size: cover;
   background-repeat: no-repeat;
